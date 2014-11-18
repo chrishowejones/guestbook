c6ad184f28e82c621f79f10978dd0b7e83581944
Chris Howe-Jones <chris.howejones@gmail.com>
2014-11-18 07:53:40 +0000

    Fixed tests.

--- test/guestbook/test/handler.clj
+++ test/guestbook/test/handler.clj
@@ -1,14 +1,21 @@
 (ns guestbook.test.handler
   (:use clojure.test
         ring.mock.request
-        guestbook.handler))
+        guestbook.handler)
+  (:require [guestbook.models.db :as db]))
 
 (deftest test-app
-  (testing "main route"
-    (let [response (app (request :get "/"))]
-      (is (= (:status response) 200))
-      (is (= (:body response) "Hello World"))))
+  (let [test-date (java.util.Date.)]
+    (with-redefs [db/read-guests (fn [] [{:name "Chris" :message "Hello" :timestamp test-date}])]
+      (testing "main route"
+        (let [response (app (request :get "/"))]
+          (is (= (:status response) 200))
+          (is (.contains (:body response) "<cite>Chris</cite>"))
+          (is (.contains (:body response) "<blockquote>Hello</blockquote>"))
+          (is (.contains (:body response) (str "<time>" (.format (java.text.SimpleDateFormat. "dd/MM/yyyy") test-date) "</time>")))))))
 
   (testing "not-found route"
     (let [response (app (request :get "/invalid"))]
       (is (= (:status response) 404)))))
+
+
